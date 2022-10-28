b9d0617b4849a58175ebdebd5684b4c7842a18e3
AleixMg <113508129+AleixMg@users.noreply.github.com>
2022-10-28 14:07:37 +0200

    Exercicis Ap2 Aleix Muñoz
    Exercicis Ap2 Aleix Muñoz

---
+++ Ap2/Ap2Ex2/src/cat/institumvm/Ap2Ex2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ *
+ * Aleix Muñoz Gálvez 
+ * Nom:Aleix
+ * Cognoms: Muñoz Gálvez
+ * INS Manuel Vázquez Montalbán
+ * Data edició: 26/10/2022
+ * Nom de cicle formatiu: Administració de sistemes infórmatics en xarxa 
+ * Nom mòdul: M03 Programació 
+ */
+package cat.institumvm;
+
+
+public class Ap2Ex2 {
+
+    private static final String MSG_1 = " Hola mon";
+    
+    public static void main(String[] args) {
+        System.out.println(MSG_1);
+    }
+    
+}
