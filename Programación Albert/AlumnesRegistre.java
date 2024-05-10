f6390d0ab18bf8602573b440b26e97017cdafb32
AleixMg <113508129+AleixMg@users.noreply.github.com>
2024-05-10 20:27:35 +0200

    Update AlumnesRegistre.java, primer constructor
    Creem el primer  constructor de la classe i inicialitzem les varibles d'instacia i l'array per a la llista de materies.

--- Programación Albert/AlumnesRegistre.java
+++ Programación Albert/AlumnesRegistre.java
@@ -13,3 +13,17 @@ class Alumne {
     private ArrayList<String> materia;
     private int edat;
     //</editor-fold>
+
+    //Fem un constructor de la classe i inicialitzem les varibles d'instacia i l'array per a la llista de materies.
+    //<editor-fold defaultstate="collapsed" desc="Constructor">
+    public Alumne(String DNI_alumne ,String nom, String cognom, int edat) {          
+        //el id de l'alumne s'hi incrementara cada cop que sa'fegeixi un 
+        this.DNI_alumne = DNI_alumne;
+        this.nom = nom;
+        this.cognom = cognom;
+        this.edat = edat;
+        this.materia = new ArrayList<>();
+     }
+     //</editor-fold>
+    
+
