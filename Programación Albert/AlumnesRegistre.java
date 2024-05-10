57ff9cd3f1551b2b4705156b691df51f4842cf7e
AleixMg <113508129+AleixMg@users.noreply.github.com>
2024-05-10 20:25:28 +0200

    Create AlumnesRegistre.java
    Creem el document important tots el paquets que utilitzarem, a més comencem declarant cada variable dels alumnes

---
+++ Programación Albert/AlumnesRegistre.java
@@ -0,0 +1,15 @@
+package alumnesregistre;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Alumne {   
+    
+    // Anem a declarar cada variable de l'alumne.
+    //<editor-fold defaultstate="collapsed" desc="Atributs">
+    private String DNI_alumne;    
+    private String nom,cognom;
+    //Creem un array per guardar les materies que vulgui afegir l'usuari
+    private ArrayList<String> materia;
+    private int edat;
+    //</editor-fold>
