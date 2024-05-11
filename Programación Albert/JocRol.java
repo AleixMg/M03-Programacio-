c0cef42eaca1b50eb818b4b1ef19210d74de8a88
AleixMg <113508129+AleixMg@users.noreply.github.com>
2024-05-11 12:28:35 +0200

    Update JocRol.java constructor d'inic de variables, getters i setters
    Declarem un constructor per inicialitzar les variables d'instancia, després declarem els getters i setters per agafar i afegir més endavant la informacio

--- Programación Albert/JocRol.java
+++ Programación Albert/JocRol.java
@@ -20,3 +20,67 @@ import org.w3c.dom.NodeList;
     private int puntsVida;
     private int puntsMana;
     //</editor-fold>
+  
+    //Fem un constructor de la classe i inicialitzem les varibles d'instacia.
+    //<editor-fold defaultstate="collapsed" desc="Constructor Personaje">
+    public Personaje(String nom, int nivell, int puntsVida, int puntsMana, String arma, String armadura) {
+        this.nom = nom;
+        this.nivell = nivell;
+        this.puntsVida = puntsVida;
+        this.puntsMana = puntsMana;
+        this.arma = arma;
+        this.armadura = armadura;
+    }
+    //</editor-fold>
+    
+    //Farem uns metodes d'accés a les varibles de l'alumne amb els getters i per afigir mes endavant l'informació els setters.
+    //<editor-fold defaultstate="collapsed" desc="Getters i Setters">
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNivel() {
+        return nivell;
+    }
+
+    public void setNivell(int nivell) {
+        this.nivell = nivell;
+    }
+
+    public int getPuntsVida() {
+        return puntsVida;
+    }
+
+    public void setPuntsVida(int puntsVida) {
+        this.puntsVida = puntsVida;
+    }
+
+    public int getPuntsMana() {
+        return puntsMana;
+    }
+
+    public void setPuntsMana(int puntsMana) {
+        this.puntsMana = puntsMana;
+    }
+    
+    public String getArma() {
+        return this.arma;
+    }
+    
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+    
+    public String getArmadura() {
+        return this.armadura;
+    }
+    
+    public void setArmadura(String armadura) {
+        this.armadura = armadura;
+    }
+    //</editor-fold>
+}
