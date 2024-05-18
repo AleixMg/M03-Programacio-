0f1a8deb2173955cf4fe3e68f3519f78ca3a7c74
AleixMg <113508129+AleixMg@users.noreply.github.com>
2024-05-18 11:53:08 +0200

    Create tasques.java Inici
    Declarem tots els paquets e inicialitzem els atributs de la clase tasca que més endavant utilitzarem(NOTA: vaig intentar utilitzar en el estat ferlo com enum per escollir unicament les 3 opcions, pero hem van sortir molts errors i no ho voug poder fer)

---
+++ Programación Albert/tasques.java
@@ -0,0 +1,14 @@
+//Utilitzare una estructura similiar a la practica que vaig entregar de AlumnesRegistre i al Joc de Rol
+package tasques;
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Tasca {
+    // Anem a declarar cada variable del personatge.
+    //<editor-fold defaultstate="collapsed" desc="Atributs">
+    private String titol,descripcio,estat;
+    private LocalDate data;
+    //</editor-fold>
