f7239be7d0f8a7eba7cbd7c87400bf5bf3b3452d
AleixMg <113508129+AleixMg@users.noreply.github.com>
2024-05-18 11:55:28 +0200

    Update tasques.java Constructor clase Tasca amb getters i setters
    Declarem el constructor de la clase Tasca amb els seus atributs que abans haviem declarat, després fem els getters i setters per a més endavant agafar i modificar les dades, afegim també un to string per a convertir-ho en una cadena de caracters

--- Programación Albert/tasques.java
+++ Programación Albert/tasques.java
@@ -12,3 +12,55 @@ class Tasca {
     private String titol,descripcio,estat;
     private LocalDate data;
     //</editor-fold>
+
+    /Fem un constructor de la classe i inicialitzem les varibles d'instacia.
+    //<editor-fold defaultstate="collapsed" desc="Constructor Tasca">
+    public Tasca(String titol, String descripcio, LocalDate data, String estat) {
+        this.titol = titol;
+        this.descripcio = descripcio;
+        this.data = data;
+        this.estat = estat;
+    }
+    //</editor-fold>
+    
+    //Farem uns metodes d'accés a les varibles de la tasca amb els getters i per afigir mes endavant l'informació els setters.
+    //<editor-fold defaultstate="collapsed" desc="Getters i Setters">
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    
+    public String getDescripcio() {
+        return descripcio;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+
+    public String getEstat() {
+        return estat;
+    }
+
+    public void setEstat(String estat) {
+        this.estat = estat;
+    }
+    
+    //Fem un toString per convertir la informació en una cade de text, ho seprearem amb comes
+    @Override
+    public String toString() {
+        return titol + "," + descripcio + "," + data + "," + estat;
+    }
+    //</editor-fold>
+}
