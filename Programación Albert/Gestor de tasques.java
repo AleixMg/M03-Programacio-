5d67a0e5ac8cb1feddd272aee2c52adc230e8bdc
AleixMg <113508129+AleixMg@users.noreply.github.com>
2024-05-23 10:15:32 +0200

    Gestor de tasques.java Importacio packets i declaracio calse tasca
    Comencem declarant els packets que anem a utilitzar amb la calse tasca i el seus atributs, posem un contador per a poder autoincrementar de forma automatica la id

---
+++ Programación Albert/Gestor de tasques.java
@@ -0,0 +1,16 @@
+package taskmanagement;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Tasca {
+	// Anem a declarar cada variable del personatge.
+	//<editor-fold defaultstate="collapsed" desc="Atributs">
+	private static int cont = 0;
+	private int id;
+	private String descripcio;
+	private boolean tasca_completada;
+	private String prioritat;
+	//</editor-fold>
+
