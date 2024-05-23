c7720fb52e3aa869957f6deaf4db6e115268448f
AleixMg <113508129+AleixMg@users.noreply.github.com>
2024-05-23 10:20:05 +0200

    Update Gestor de tasques.java getters
    Afegim els getters  dels atributs abans declarats amb un to string per a convertir la informació en cadena de text

--- Programación Albert/Gestor de tasques.java
+++ Programación Albert/Gestor de tasques.java
@@ -23,4 +23,37 @@ class Tasca {
     	this.tasca_completada = false;
     	this.prioritat = prioritat;
 	}
+	//</editor-fold>
+
+ 	//Farem uns metodes d'accés a les varibles de la tasca amb els getters. 
+	//<editor-fold defaultstate="collapsed" desc="Getters">
+	public void marcarTascaCompletada() {
+    	this.tasca_completada = true;
+	}
+
+	public int getID() {
+    	return this.id;
+	}
+
+	public String getDescripcio() {
+    	return this.descripcio;
+	}
+
+	public boolean estaCompletada() {
+    	return this.tasca_completada;
+	}
+
+	public String getPrioritat() {
+    	return this.prioritat;
+	}
+
+	//Fem un toString per convertir la informació en una cadena de text, ho seprearem amb tabulacions i dos |.
+	@Override
+	public String toString() {
+    	String estat_tasca = tasca_completada ? "Completada" : "Pendent";
+    	return "ID: " + id + "\t||Descripcio: " + descripcio + "\t||Prioritat: " + prioritat + "\t||Estat: " + estat_tasca;
+	}
+	//</editor-fold>
+}
+
 
