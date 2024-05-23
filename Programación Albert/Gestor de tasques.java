758936c144497999d7d22038db1c29c41a0e537d
AleixMg <113508129+AleixMg@users.noreply.github.com>
2024-05-23 10:17:01 +0200

    Update Gestor de tasques.java constructor de la clase tasca
    Declarem el constructor de la clase tasca o inicialitzem, les variables i fem un autoincrement del contador per la id

--- Programación Albert/Gestor de tasques.java
+++ Programación Albert/Gestor de tasques.java
@@ -13,4 +13,14 @@ class Tasca {
 	private boolean tasca_completada;
 	private String prioritat;
 	//</editor-fold>
+	
+	//Fem un constructor de la classe i inicialitzem les varibles d'instacia.
+  	//<editor-fold defaultstate="collapsed" desc="Constructor Tasca">
+	public Tasca(String descripcio, String prioritat) {
+	//al contador li fiquem un autoincrement per a que autoincrementi la id de forma sola
+    	this.id = ++cont;
+    	this.descripcio = descripcio;
+    	this.tasca_completada = false;
+    	this.prioritat = prioritat;
+	}
 
